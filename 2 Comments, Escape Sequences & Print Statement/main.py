995bc62de4becd0adb205e03c853255ccfb7b8ac
Ayush Dhiman <ayushdhiman@Ayushs-MacBook-Pro.local>
2026-03-30 18:41:26 +0530

    Comments, Escape Sequences & Print Statement Completed

---
+++ 2 Comments, Escape Sequences & Print Statement/main.py
@@ -0,0 +1,14 @@
+'''
+This is a comment
+Author: Ayush
+Course: 100 Days of code
+'''
+print("Hey I am a \"good boy\"\nand this viewer is also a good boy")
+
+print("Hello World")
+# This is a Single-Line Comment
+print("This is a print statement.")
+print("Hello World !!!") # Printing Hello World
+
+print("Hey", 6, 7, sep= "~", end = "009\n")
+print("Ayush")
