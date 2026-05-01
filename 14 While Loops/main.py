67fb314a36e24877b8a3224f4ab7053782ed606c
Ayush Dhiman <ayushdhiman@Ayushs-MacBook-Pro.local>
2026-05-01 12:01:49 +0530

    12 Match Case/ 	13 For Loops/ 	14 While Loops/ 	15 Break and Continue/completed

---
+++ 14 While Loops/main.py
@@ -0,0 +1,31 @@
+# for i in range(3):
+#     print(i)
+
+# i = 0
+
+# while(i <= 3):
+#     print(i)
+#     i += 1
+
+# print("Done with loop")
+
+# i = int(input("Enter the number: "))
+# print(i)
+# while(i <= 38):
+#     i = int(input("Enter the number: "))
+#     print(i)
+
+# print("Done with loop")
+
+# while(True):
+#     i = int(input("Enter the number: "))
+#     print(i)
+#     if not i <= 38:
+#         break
+
+count = 5
+while(count > 0):
+    print(count)
+    count = count - 1
+else:
+    print("I am inside else")
---
+++ 15 Break and Continue/main.py
@@ -0,0 +1,15 @@
+for i in range(12):
+    if(i==10):
+        print("Skip the iteration")
+        continue
+    print("5 X",i+1, "=", 5 * (i+1))
+
+print("Done with execution")
+
+# for i in range(1,101,1):
+#     print(i, end= " ")
+#     if i == 50:
+#         break
+#     else:
+#         print("Mississipp")
+# print("Thank You")
